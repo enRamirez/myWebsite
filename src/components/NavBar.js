d93fd754b95556ea67fd659b807d509b9d41532f
enRamirez <ricky.ramirez125042@gmail.com>
2024-04-10 22:03:18 -0500

    added banner and starting on skills

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import {Nav, Navbar, Container} from "react-bootstrap"
 import logo from '../assets/img/Logo.png'
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon2 from '../assets/img/nav-icon2.svg'
+import navIcon3 from '../assets/img/nav-icon3.svg'
 
 export const NavBar = () => {
     const {activeLink, setActiveLink} = useState('home');
@@ -17,7 +20,7 @@ export const NavBar = () => {
 
         window.addEventListener("scroll", onScroll);
         
-        return () => window.removeEventListener("scroll", onScroll)
+        return () => window.removeEventListener("scroll", onScroll);
     }, [])
 
     const onUpdateActiveLink = (value) => {
@@ -42,9 +45,9 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                <a href=""><img src={navIcon1} alt="" /></a>
-                <a href=""><img src={navIcon2} alt="" /></a>
-                <a href=""><img src={navIcon3} alt="" /></a>
+                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
           </span>
