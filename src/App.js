9eddf87c43f66f64e43b528d462fed6017507d04
enRamirez <ricky.ramirez125042@gmail.com>
2024-07-12 21:01:22 -0500

    cleaned up code, next is business

--- src/App.js
+++ src/App.js
@@ -1,60 +1,24 @@
-import React, { useState } from 'react';
+// import logo from './logo.svg';
+import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
-import { Skills } from './components/Skills';
-import { Projects } from './components/Projects';
+import { Skills } from './components/Skills'
+import { Projects } from './components/Projects'
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  const [currentComponent, setCurrentComponent] = useState('home');
-
   return (
     <div className="App">
-      <NavBar setCurrentComponent={setCurrentComponent} />
-      {currentComponent === 'home' && (
-        <>
-          <Banner setCurrentComponent={setCurrentComponent} />
-          <Skills />
-          <Projects />
-        </>
-      )}
-      {currentComponent === 'skills' && <Skills />}
-      {currentComponent === 'projects' && <Projects />}
-      {currentComponent === 'contact' && <Contact />}
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
       <Footer />
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-// import logo from './logo.svg';
-// import './App.css';
-// import { NavBar } from './components/NavBar';
-// import { Banner } from './components/Banner';
-// import { Skills } from './components/Skills'
-// import { Projects } from './components/Projects'
-// import { Footer } from './components/Footer';
-// import 'bootstrap/dist/css/bootstrap.min.css'
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <NavBar />
-//       <Banner />
-//       <Skills />
-//       <Projects />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,11 @@
-import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import colorSharp2 from "../assets/img/color-sharp2.png"
+// import projImg1 from "../assets/img/project-img1.png";
+// import projImg2 from "../assets/img/project-img2.png";
+// import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
 
@@ -30,50 +35,64 @@ export const Projects = () => {
             description: "Design & Development",
             // img: projImg5,
         },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            // img: projImg5,
+        },
     ];
 
     return (
         <section className="project" id="projects">
-            <Container>
-                <Row>
-                    <Col>
-                        <h2>Projects</h2>
-                        <p>text holder</p>
-                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                        <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                            <Nav.Item>
-                                <Nav.Link eventKey="first">Tab One</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="second">Tab Two</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="third">Tab Three</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                        <Tab.Content>
-                            <Tab.Pane eventKey="first">
-                                <Row>
-                                    {
-                                        projects.map((project, index) => {
-                                            return (
-                                                <ProjectCard 
-                                                key={index}
-                                                {...project}
-                                                />
-                                            )
-                                        })
-                                    }
-                                </Row>
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="second">text holder</Tab.Pane>
-                            <Tab.Pane eventKey="third">text holder</Tab.Pane>
-                        </Tab.Content>
-                        </Tab.Container>
-                    </Col>
-                </Row>
-            </Container>
-            <img className="background-image-right" alt="" src={colorSharp2}></img>
+          <Container>
+            <Row>
+              <Col size={12}>
+                <TrackVisibility>
+                  {({ isVisible }) =>
+                  <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                    <h2>Projects</h2>
+                    <p>Text Holder</p>
+                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                      <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                        <Nav.Item>
+                          <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                        </Nav.Item>
+                      </Nav>
+                      <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                        <Tab.Pane eventKey="first">
+                          <Row>
+                            {
+                              projects.map((project, index) => {
+                                return (
+                                  <ProjectCard
+                                    key={index}
+                                    {...project}
+                                    />
+                                )
+                              })
+                            }
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second">
+                          <p>Text Holder</p>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="third">
+                          <p>Text Holder</p>
+                        </Tab.Pane>
+                      </Tab.Content>
+                    </Tab.Container>
+                  </div>}
+                </TrackVisibility>
+              </Col>
+            </Row>
+          </Container>
+          <img className="background-image-right" alt="" src={colorSharp2}></img>
         </section>
-    )
-}
+      )
+    }
